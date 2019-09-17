885b0b0f7ac7e45a41ac3a6a3372cb066901ea1b
Kjetil <kjetil.dir@gmail.com>
2019-09-17 16:00:24 +0200

    refactored host

--- src/test/java/HttpClient.java
+++ src/test/java/HttpClient.java
@@ -2,24 +2,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
-public class HttpClient {
+class HttpClient {
 
+
+    private String host;
+
+    public HttpClient(String host) {
+        this.host = host;
+    }
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("urlecho.appspot.com", 80);
+        new HttpClient("urlecho.appspot.com").executeRequest();
+    }
 
-        socket.getOutputStream().write("GET /echo?status=200&Content-Type=text%2Fhtml&body=Hello%20world! HTTP/1.1\r\n"
-        .getBytes());
-        socket.getOutputStream().write("Host: urlecho.appspot.com\r\n".getBytes());
-        socket.getOutputStream().write("connection: close\r\n".getBytes());
-        socket.getOutputStream().write("\n\r".getBytes());
-        socket.getOutputStream().flush();
+    private void executeRequest() throws IOException {
+        try(Socket socket = new Socket(host, 80)) {
 
-        InputStream input = socket.getInputStream();
-        int c = input.read();
-        while (c != -1) {
-            System.out.print((char)c);
-            c = input.read();
+            socket.getOutputStream().write("GET /echo?status=200&Content-Type=text%2Fhtml&body=Hello%20world! HTTP/1.1\r\n"
+                    .getBytes());
+            socket.getOutputStream().write(("Host:" + host + "\r\n").getBytes());
+            socket.getOutputStream().write("connection: close\r\n".getBytes());
+            socket.getOutputStream().write("\n\r".getBytes());
+            socket.getOutputStream().flush();
+
+            InputStream input = socket.getInputStream();
+            int c;
+            while ((c = input.read()) != -1) {
+                System.out.print((char) c);
+            }
         }
     }
 }
